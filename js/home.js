9cbf1f7773a89b76e27449266ea2996391c95530
Aantara21 <antarabaidya21@gmail.com>
2024-10-14 11:52:49 +0600

    commit 2nd step

---
+++ js/home.js
@@ -0,0 +1,4 @@
+document.getElementById('home-button').addEventListener('click', function (event) {
+    console.log('home button clicked');
+    window.location.href = 'index.html'
+})
--- js/payoo.js
+++ js/payoo.js
@@ -3,5 +3,17 @@ document.getElementById('login-button').addEventListener('click', function (even
     console.log('button added');
 
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+
+    if (phoneNumber === '5' && pinNumber === '1234') {
+        console.log('you are logged in');
+        window.location.href = 'home.html'
+
+    }
+    else {
+        alert('wrong information');
+    }
 })
+
+ 
