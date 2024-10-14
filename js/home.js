6a6af5197dc2f523200b326e9a80af841799ddff
Aantara21 <antarabaidya21@gmail.com>
2024-10-14 16:05:08 +0600

    cash in cash out done

--- js/home.js
+++ js/home.js
@@ -4,6 +4,47 @@ document.getElementById('add-money-button')
         event.preventDefault();
 
         const moneygive = document.getElementById('input-give-amount').value;
+        const moneygivenumber = parseFloat(moneygive);
         const inputPinNumber = document.getElementById('input-pin-number').value;
         console.log(inputPinNumber);
+
+        if (inputPinNumber === "1234") {
+            const currentBalance = document.getElementById('main-balance').innerText;
+            const balanceNumber = parseFloat(currentBalance);
+
+            const newBalance = moneygivenumber + balanceNumber;
+
+            document.getElementById('main-balance').innerText = newBalance;
+        }
+        else {
+            alert('wrong information');
+        }
+    })
+
+document.getElementById('cashout-button')
+    .addEventListener('click', function name(event) {
+        event.preventDefault();
+
+
+
+        const cashoutpin = document.getElementById('cashout-pin').value;
+        const cashoutAmount = document.getElementById('cashout-amount').value;
+        const finalcashoutamount = parseFloat(cashoutAmount);
+
+        if (cashoutpin === '12345') {
+            const balance = document.getElementById('main-balance').innerText;
+            const finalBalance = parseFloat(balance);
+            
+
+            const cashmoney = finalBalance - finalcashoutamount;
+            console.log(cashmoney);
+
+
+            document.getElementById('main-balance').innerText = cashmoney;
+
+        }
+        else {
+            alert('wrong input information');
+        }
+
     })
